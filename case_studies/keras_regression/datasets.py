cb48b927e87984d9099540a9332a28d3356a6af7
Guilherme Strachan <guilherme.strachan@engie.com>
2019-01-29 17:15:47 -0200

    load image data for keras regression

--- case_studies/keras_regression/datasets.py
+++ case_studies/keras_regression/datasets.py
@@ -38,3 +38,29 @@ def process_house_attributes(df, train, test):
     test_x = np.hstack([test_continuous, test_categorical])
 
     return (train_x, test_x)
+
+
+def load_house_images(df, path):
+
+    images = []
+
+    for i in df.index.values:
+        base_path = os.path.sep.join([path, f'{i + 1}_*'])
+        house_paths = sorted(list(glob.glob(base_path)))
+
+        input_images = []
+        output_image = np.zeros((64, 64, 3), dtype='uint8')
+
+        for house_path in house_paths:
+            image = cv2.imread(house_path)
+            image = cv2.resize(image, (32, 32))
+            input_images.append(image)
+
+        output_image[0:32, 0:32] = input_images[0]
+        output_image[0:32, 32:64] = input_images[1]
+        output_image[32:64, 32:64] = input_images[2]
+        output_image[32:64, 0:32] = input_images[3]
+
+        images.append(output_image)
+
+    return np.array(images)
