850bb52d70ff5b0554e442623cfebe56bce26578
Guilherme Strachan <guilherme.strachan@engie.com>
2019-01-29 17:16:08 -0200

    add cnn for keras regression

--- case_studies/keras_regression/models.py
+++ case_studies/keras_regression/models.py
@@ -19,3 +19,36 @@ def create_mlp(dim, regress=False):
         model.add(Dense(1, activation='linear'))
 
     return model
+
+
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
+
+    input_shape = (height, width, depth)
+    chan_dim = -1
+
+    inputs = Input(shape=input_shape)
+
+    for(i, f) in enumerate(filters):
+        if i == 0:
+            x = inputs
+
+        x = Conv2D(f, (3, 3), padding='same')(x)
+        x = Activation('relu')(x)
+        x = BatchNormalization(axis=chan_dim)(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = Flatten()(x)
+    x = Dense(16)(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization(axis=chan_dim)(x)
+    x = Dropout(0.5)(x)
+
+    x = Dense(4)(x)
+    x = Activation('relu')(x)
+
+    if regress:
+        x = Dense(1, activation='linear')(x)
+
+    model = Model(inputs, x)
+
+    return model
